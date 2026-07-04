c114f23eab29f854fe20d2287eb6d2bdd4dd857d
Pro3dros <nik.ventouris@gmail.com>
2026-07-04 23:42:19 +0300

    Fix dependency mismatch

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from "@tanstack/start/config";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      viteReact(),
-      tsconfigPaths(),
-    ],
-  },
-  server: {
-    preset: "netlify", // Εδώ λέμε στο Netlify πώς να χτίσει το site
-  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
 });
